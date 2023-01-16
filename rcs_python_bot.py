054a9e6f680de1ff1a81633becde1b7e27e60266
davakm <114732909+davakm@users.noreply.github.com>
2023-01-16 12:59:07 +0200

    Update rcs_python_bot.py

--- rcs_python_bot.py
+++ rcs_python_bot.py
@@ -1,24 +1,51 @@
-import telebot
-from telebot import types
+@bot.message_handler(commands=['webdev'])
+def mess(message):
+    get_message_bot = message.text.strip().lower()
 
-bot = telebot.TeleBot('')
+    if get_message_bot == "webdev":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = types.KeyboardButton("FRONT END")
+        btn2 = types.KeyboardButton("BACK END")
+        markup.add(btn1, btn2)
+        last_message = "Please choose one of two options"
 
-# Make bot's reaction on word "start" / User can choose from 6 variants
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    btn1 = types.KeyboardButton('GAMES')
-    btn2 = types.KeyboardButton('MOBILE APP')
-    btn3 = types.KeyboardButton('WEB DEV')
-    btn4 = types.KeyboardButton('SOFT')
-    btn5 = types.KeyboardButton('DATA ANALYTICS')
-    btn6 = types.KeyboardButton('AI')
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = types.KeyboardButton('GAMES')
+        btn2 = types.KeyboardButton('MOBILE APP')
+        btn3 = types.KeyboardButton('WEB DEV')
+        btn4 = types.KeyboardButton('DATA ANALYTICS')
+        markup.add(btn1, btn2, btn3, btn4, )
+        last_message = "Choose one of the options"
+    bot.send_message(message.chat.id, last_message, parse_mode='html', reply_markup=markup)
 
-    send_mess = f"<b>Hello {message.from_user.first_name}</b>\nWhat direction are you interested in?"
-    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
+@bot.message_handler(commands=['analytics'])
+def mess(message):
+    get_message_bot = message.text.strip().lower()
+
+    if get_message_bot == "analytics":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = types.KeyboardButton("DATA SCIENCE")
+        btn2 = types.KeyboardButton("MACHINE LEARNING")
+        markup.add(btn1, btn2)
+        last_message = "Please choose one of two options"
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = types.KeyboardButton('GAMES')
+        btn2 = types.KeyboardButton('MOBILE APP')
+        btn3 = types.KeyboardButton('WEB DEV')
+        btn4 = types.KeyboardButton('DATA ANALYTICS')
+        markup.add(btn1, btn2, btn3, btn4, )
+        last_message = "Please choose one of the options"
+    bot.send_message(message.chat.id, last_message, parse_mode='html', reply_markup=markup)
+
+# https: // www.w3schools.com / howto / howto_blog_become_frontenddev.asp
+# https://learntocodewith.me/posts/backend-development/
+
+# https://www.edx.org/learn/data-science
+# https://www.coursera.org/learn/machine-learning
 
 # start bot
 bot.polling(none_stop=True)
